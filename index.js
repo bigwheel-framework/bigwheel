a1c6c77f7baf275ed6940bf7cbbeecc713632b0c
Mikko Haapoja <me@mikkoh.com>
2014-09-28 20:52:42 -0400

    Externalized the router

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-//TODO: separate out router just for future maintainability
-
 // settings
 // postHash
 // autoResize
+// onRoute
 // routes start with / you can define a 404
 
 var vm = require( 'bw-vm' ),
-	viewmediator = require( 'bw-viewmediator' ), 
-	eventlistener = require( 'eventlistener' ),
-	routes = require( 'routes' );
+	viewmediator = require( 'bw-viewmediator' ),
+	router = require( 'bw-router' ),
+	eventlistener = require( 'eventlistener' );
 
 function bigwheel( settings ) {
 
@@ -17,10 +16,14 @@ function bigwheel( settings ) {
 
 	var s = this.s = settings || {};
 
-	s.postHash = s.postHash || '!';
 	s.autoResize = s.autoResize === undefined ? true : s.autoResize;
 
-	this.router = routes();
+	// setup the router
+	this.onRouteCallBack = settings.onRoute;
+	settings.onRoute = this.show.bind( this );
+	this.router = router( settings );
+
+	// setup the view manager
 	this.vm = vm( this.s );
 
 	if( s.autoResize )
@@ -31,69 +34,30 @@ bigwheel.prototype = {
 
 	init: function() {
 
-		var s = this.s,
-			noop = function() {};
-
-		// figure out a start section
-		if( s[ '/' ] === undefined ) {
-
-			// find the first path which would be a section
-			for( var i in s ) {
-
-				if( i[ 0 ] == '/' ) {
-
-					s.start = i;
-
-					break;
-				}
-			}
-		} else {
-
-			s.start = '/';
-		}
-
-
-		// now setup routes
-		for( var i in s ) {
-
-			if( i[ 0 ] == '/' ) {
-
-				this.router.addRoute( i, noop );
-			}
-		}
-
-		eventlistener.add( window, 'hashchange', this.onURL.bind( this ) );
-
-		if( s.autoResize )
-			eventlistener.add( window, 'resize', this.onResize.bind( this ) );
-
-		// force a hash change to start things up
-		this.onURL();
+		this.router.init();
 
 		return this;
 	},
 
 	add: function( route, section ) {
 
-		var s = this.s;
-
-		s[ route ] = section;
+		this.router.add( route, section );
 
 		return this;
 	},
 
 	go: function( to ) {
 
-		if( to[ 0 ] != '/' )
-			to = '/' + to;
-
-		window.location.hash = this.s.postHash + to;
+		this.router.go( to );
 	},
 
 	show: function( content, data ) {
 
-		//check if content is an array or function or object
-		
+		// this is the original router callback passed in
+		if( this.onRouteCallBack )
+			this.onRouteCallBack( content, data );
+
+		// check if content is an array or function or object
 		if( Array.isArray( content ) ) {
 
 			var contents = [];
@@ -126,40 +90,9 @@ bigwheel.prototype = {
 		this.vm.show( content, data );
 	},
 
-	doRoute: function( data ) {
-
-		var s = this.s,
-			section;
-
-		if( data ) {
-
-			section = s[ data.route ];
-		}
-
-		if( section ) {
-
-			this.show( section, data );
-		} else if( s[ '404' ] ) {
-
-			this.show( s[ '404' ], data );
-		}
-	},
-
 	onResize: function() {
 
 		this.resize( window.innerWidth, window.innerHeight );
-	},
-
-	onURL: function() {
-
-		var cRoute = '/';
-
-		if( window.location.hash != '' ) {
-
-			cRoute = window.location.hash.substr( 1 + this.s.postHash.length );
-		}
-
-		this.doRoute( this.router.match( cRoute ) );
 	}
 };
 
