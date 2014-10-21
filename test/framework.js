8c93d047367ce3d78866f9f98dbe652edfd5e882
Mikko Haapoja <me@mikkoh.com>
2014-10-21 11:45:47 -0400

    testing with callback

--- test/framework.js
+++ test/framework.js
@@ -2,29 +2,35 @@ var promise = require( 'promise' ),
 	bigwheel = require( '../' ),
 	testSection = require( './section' );
 
-// var fw = bigwheel();
-// 
-// fw.add( '/', testSection( 'info', '#00CAFE', fw, '/about' ) );
-// fw.add( '/about', testSection( 'about', '#CAFE00', fw, '/contact' ) );
-// fw.add( '/contact', testSection( 'contact', '#CA00FE', fw, '/' ) );
+// module.exports = bigwheel( function() {
 
-module.exports = bigwheel( function() {
+// 	return new promise( function( onOk, onErr ) {
 
-	return new promise( function( onOk, onErr ) {
+// 		onOk( {
 
-		onOk( {
+// 			'/': testSection( '#00CAFE', '/about' ),
 
-			'/': testSection( '#00CAFE', '/about' ),
+// 			'/about': [ testSection( 'rgba( 255, 0, 0, 0.5 )', '/contact' ), 
+// 						testSection( 'rgba( 0, 0, 255, 0.5 )', '/contact' ) ],
 
-			'/about': [ testSection( 'rgba( 255, 0, 0, 0.5 )', '/contact' ), 
-						testSection( 'rgba( 0, 0, 255, 0.5 )', '/contact' ) ],
+// 			'/contact': testSection( '#CA00FE', '/' ),
+// 			'404': testSection( '#CA0000', '/' )
+// 		});
+// 	});
 
-			// fancy: model( bigwheelbars )( container )( genericview ),
-			// // fancy: model( bigwheelbars )( container( '#thing' ) )( genericview ),
+// }).init();
 
-			'/contact': testSection( '#CA00FE', '/' ),
-			'404': testSection( '#CA0000', '/' )
-		});
+
+module.exports = bigwheel( function( cb ) {
+
+	cb( {
+
+		'/': testSection( '#00CAFE', '/about' ),
+
+		'/about': [ testSection( 'rgba( 255, 0, 0, 0.5 )', '/contact' ), 
+					testSection( 'rgba( 0, 0, 255, 0.5 )', '/contact' ) ],
+
+		'/contact': testSection( '#CA00FE', '/' ),
+		'404': testSection( '#CA0000', '/' )
 	});
-	
 }).init();
