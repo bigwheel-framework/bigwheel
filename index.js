14dc083a4d5b7d31bf99574636726e3ac20467c4
Nick <nick@jam3.com>
2015-01-06 14:51:44 -0500

    - Updated duplicate check

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ bigwheel.prototype = {
 				throw new Error( 'Your settings object must define routes' );
 
 			s.autoResize = s.autoResize === undefined ? true : s.autoResize;
-			s.duplicate = s.duplicate === undefined ? true : s.duplicate;
 
 			// setup the router
 			this.onRouteCallBack = settings.onRoute;
@@ -124,8 +123,7 @@ bigwheel.prototype = {
 	},
 
 	show: function( content, data ) {
-		
-		if (!this.s.duplicate && data && data.route==this.lastRoute) return;
+		if (data && !data.duplicate && data.route==this.lastRoute) return;
 		// this is the original router callback passed in
 		if( this.onRouteCallBack )
 			this.onRouteCallBack( content, data );
